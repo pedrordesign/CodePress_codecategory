9e10c9b7943308e9adf0866758e9845c16b84e12
Pedro Ramos <pedroramos@sitehosting.com.br>
2016-11-08 10:34:43 -0200

    adjusting routes & installing codeuser

--- src/CodeCategory/routes.php
+++ src/CodeCategory/routes.php
@@ -4,12 +4,14 @@ Route::group([
         'prefix' => 'admin/categories',
         'as' => 'admin.categories.',
         'namespace'=>'CodePress\CodeCategory\Controllers',
-        'middleware' => ['web']
+        'middleware' => ['web', 'auth']
     ],
     function(){
 
-    Route::get('', [ 'uses' => 'AdminCategoriesController@index', 'as' => 'index']);
-    Route::get('/create', [ 'uses' => 'AdminCategoriesController@create', 'as' => 'create']);
-    Route::post('/store', [ 'uses' => 'AdminCategoriesController@store', 'as' => 'store']);
+        Route::get('', [ 'uses' => 'AdminCategoriesController@index', 'as' => 'index']);
+        Route::get('/create', [ 'uses' => 'AdminCategoriesController@create', 'as' => 'create']);
+        Route::post('/store', [ 'uses' => 'AdminCategoriesController@store', 'as' => 'store']);
+        Route::get('{id}/edit', [ 'uses' => 'AdminCategoriesController@edit', 'as' => 'edit']);
+        Route::put('{id}/update', [ 'uses' => 'AdminCategoriesController@update', 'as' => 'update']);
 
-});
+    });
