fb0d1d9f95ba0b6a0d347fa4297319b2c5d4e557
Pedro Ramos <pedroramos@sitehosting.com.br>
2016-11-10 08:54:08 -0200

    MIGRATION with user_id

--- src/resources/migrations/2016_10_27_183209_create_code_categories_table.php
+++ src/resources/migrations/2016_10_27_183209_create_code_categories_table.php
@@ -20,6 +20,8 @@ class CreateCodeCategoriesTable extends Migration
             $table->string('name');
             $table->string('slug');
             $table->boolean('active')->default(false);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('codepress_users');
             $table->integer('categorizable_id')->nullable();
             $table->string('categorizable_type')->nullable();
             $table->timestamps();
